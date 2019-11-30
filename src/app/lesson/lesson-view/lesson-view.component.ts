aa34bd4846b415bc2e13af6a65f7d095513f4d96
watsacca <watsacca23@gmail.com>
2019-11-30 15:06:13 +0100

    show lesson, then type blind

--- src/app/lesson/lesson-view/lesson-view.component.ts
+++ src/app/lesson/lesson-view/lesson-view.component.ts
@@ -1,9 +1,14 @@
-import {Component, ElementRef, EventEmitter, HostListener, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {LessonService} from '../lesson.service';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {Score} from '../../score/_models/scroe.model';
-import {KeyboardMapping, KeyboardMappingService} from '../keyboard-mapping.service';
 import {VelthuisService} from '../velthuis.service';
+
+enum State {
+  SHOW_LESSON,
+  TYPING,
+  SHOW_SCORE,
+}
 
 @Component({
   selector: 'app-lesson-view',
@@ -18,8 +23,7 @@ export class LessonViewComponent implements OnInit {
   readonly MAX_LESSON = LessonService.MAX_LESSON;
 
   lessonNumber = 1;
-  lesson = '';
-  cursorPos = 0;
+  lessonTyped = '';
 
   errorMessage = '';
   finishedMessage = '';
@@ -27,91 +31,86 @@ export class LessonViewComponent implements OnInit {
 
   velthuis = false;
 
-  keyboardMapping: KeyboardMapping | false = false;
-  keyboardMappingOptions: { name: string, value: KeyboardMapping }[] =
-    [{name: 'QWERTY -> Pali Ṃeāt', value: 'qwertyToPaliMeat'},
-      {name: 'QWERTZ -> Pali Ṃeāt', value: 'qwertzToPaliMeat'},
-      {name: 'QWERTY -> QWPR', value: 'qwertyToQwpr'},
-      {name: 'QWERTZ -> QWPR', value: 'qwertzToQwpr'},
-    ];
-
+  private state: State = State.SHOW_LESSON;
 
   @ViewChild('textArea', {static: true}) private textarea: ElementRef<HTMLTextAreaElement>;
 
   constructor(private activatedRoute: ActivatedRoute,
               private router: Router,
               private lessonService: LessonService,
-              private velthuisService: VelthuisService,
-              private keyboardMappingService: KeyboardMappingService) {
+              private velthuisService: VelthuisService) {
   }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(
       (params: Params) => {
-        this.finishedMessage = '';
-        this.errorMessage = '';
-        this.lessonScore = 0;
-        this.cursorPos = 0;
         this.lessonNumber = parseInt(params['lessonNumber'], 10);
         this.velthuis = this.activatedRoute.snapshot.queryParams['velthuis'] === 'true';
-        this.updateLesson();
+        this.reset();
+        this.textarea.nativeElement.focus();
       }
     );
-    this.activatedRoute.url.subscribe(() => this.reset(this.textarea.nativeElement));
+    this.activatedRoute.url.subscribe(() => this.reset());
+    this.textarea.nativeElement.focus();
   }
 
-  @HostListener('window:keypress', ['$event'])
-  onKey(event: { key: string }) {
-    const textArea = this.textarea.nativeElement;
-    let key = event.key;
+  onStartClicked() {
+    this.state = State.TYPING;
+    this.focus();
+  }
 
-    // FIXME: we need the focus to select text. Find a way to keep the selection without focus instead of graping it
-    if (this.lessonService.keepFocus) {
-      textArea.focus();
-    } else {
-      return true;
+  onDoneClicked() {
+    this.lessonTyped = this.lessonTyped.replace('\r', '');
+
+    // if (this.keyboardMapping && (this.keyboardMapping as any) !== 'false') {
+    //   key = this.keyboardMappingService.translate(key, this.keyboardMapping);
+    // }
+
+    this.lessonScore += 100 * this.lessonNumber;
+    this.finishedMessage = `Lesson completed! High score +${this.lessonScore} 🙌 `;
+    this.scoreIncrement.emit(this.lessonScore);
+    this.lessonScore = 0;
+    this.state = State.SHOW_SCORE;
+
+  }
+
+  onRestartClicked() {
+    this.reset();
+  }
+
+  makeLessonText() {
+    if (this.lessonNumber) {
+      if (this.velthuis) {
+        return this.velthuisService.substitute(this.lessonService.make(this.lessonNumber));
+      } else {
+        return this.lessonService.make(this.lessonNumber);
+      }
     }
+    return '';
+  }
 
-    // ignore windows \r ...
-    if (key === '\r') {
-      return;
-    } else if (key.toLowerCase() === 'enter') {
-      key = '\n';
-    }
-
-    if (this.keyboardMapping && (this.keyboardMapping as any) !== 'false') {
-      key = this.keyboardMappingService.translate(key, this.keyboardMapping);
-    }
-
-    if (this.finishedMessage) {
-      // TODO: do something here to communicate that the lesson is finished (shake animation for the textArea?)
-    } else if (this.matchesLessonWithSpecialKey(key, this.cursorPos)) {
-      this.cursorPos++;
-      this.correctKey(textArea, key);
-    } else if (this.matchesLesson(key, this.cursorPos)) {
-      this.correctKey(textArea, key);
-    } else {
-      this.resetAndDisplayError(textArea);
-    }
-
-    if (!this.finishedMessage && this.cursorPos >= this.lesson.length) {
-      this.lessonScore += 100 * this.lessonNumber;
-      this.finishedMessage = `Lesson completed! High score +${this.lessonScore} 🙌 `;
-      this.scoreIncrement.emit(this.lessonScore);
-      this.lessonScore = 0;
-    }
-
-    // prevent space bar from scrolling the page
-    return false;
+  makeLessonScore() {
+    return 'TODO';
   }
 
   onVelthuisChanged() {
-    this.updateLesson();
     return this.router.navigate(['lesson', this.lessonNumber], {queryParams: {velthuis: this.velthuis}, queryParamsHandling: 'merge'});
   }
 
   makeLessonLink(offset = 0) {
     return `/lesson/${this.lessonNumber + offset}`;
+  }
+
+  isShowLesson() {
+    return this.state === State.SHOW_LESSON;
+  }
+
+  isTyping() {
+    return this.state === State.TYPING;
+  }
+
+  isShowScore() {
+    return this.state === State.SHOW_SCORE;
   }
 
   // the user does not need to type the space, return or '-' button
@@ -125,42 +124,20 @@ export class LessonViewComponent implements OnInit {
   }
 
   private matchesLesson(key: string, cursorPos: number) {
-    return this.lesson.charAt(cursorPos) === key;
+    return this.lessonTyped.charAt(cursorPos) === key;
   }
 
-  // we are strict about making errors -> restart
-  private resetAndDisplayError(textArea: HTMLTextAreaElement) {
-    this.errorMessage = 'You missed a character, please start over!';
-    this.lessonScore = this.lessonScore > 20 ? this.lessonScore - 20 : 0;
-    if (this.lessonScore > 0) {
-      this.scoreIncrement.emit(this.lessonScore);
-      this.errorMessage += ` High score +${this.lessonScore} `;
-    }
-    this.reset(textArea);
-  }
-
-  private reset(textArea: HTMLTextAreaElement) {
-    textArea.setSelectionRange(0, 0);
-    this.cursorPos = 0;
+  private reset() {
+    this.lessonTyped = '';
     this.lessonScore = 0;
-  }
-
-  private correctKey(textArea: HTMLTextAreaElement, key: string) {
-    textArea.setSelectionRange(0, ++this.cursorPos);
+    this.finishedMessage = '';
     this.errorMessage = '';
-    if (key !== ' ' && key !== '\n') {
-      this.lessonScore++;
-    }
+    this.state = State.SHOW_LESSON;
   }
 
-  private updateLesson() {
-    if (this.lessonNumber) {
-      this.reset(this.textarea.nativeElement);
-      if (this.velthuis) {
-        this.lesson = this.velthuisService.substitute(this.lessonService.make(this.lessonNumber));
-      } else {
-        this.lesson = this.lessonService.make(this.lessonNumber);
-      }
+  private focus() {
+    if (this.textarea && this.textarea.nativeElement) {
+      this.textarea.nativeElement.focus();
     }
   }
 
